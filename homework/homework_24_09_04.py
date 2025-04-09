dbd02b275bfcb2b97c0fd433ed76719dad157bcd
andreew75 <anline75@gmail.com>
2025-04-10 00:29:57 +0300

    added homework

---
+++ homework/homework_24_09_04.py
@@ -0,0 +1,37 @@
+# Костромин Андрей Львович
+
+
+class Pair:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+
+class RightTriangle(Pair):
+    def __init__(self, a, b):
+        super().__init__(a, b)
+
+    def hypotenuse(self):
+        return (self.a ** 2 + self.b ** 2) ** 0.5
+
+    def square(self):
+        return (self.a * self.b) / 2
+
+    def print_info(self):
+        print(f"Прямоугольный треугольник АВС ({self.a}, {self.b}, {self.hypotenuse():.2f})")
+        print(f"Катеты: {self.a}, {self.b}")
+        print(f"Гипотенуза: {self.hypotenuse():.2f}")
+        print(f"Площадь треугольника: {self.square()}")
+        print(f"Сумма катетов: {self.add()}")
+        print(f"Произведение катетов: {self.multiply()}")
+
+
+triangle = RightTriangle(5, 8)
+triangle.print_info()
+
