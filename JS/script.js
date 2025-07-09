5a3bf34eb6492b8c4ea01924fb6fc612bebcd3d1
andreew75 <anline75@gmail.com>
2025-07-09 23:11:50 +0300

    update

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92ea2404314a45b9cb1cbb3ef11d16de319d79bdccbfd7cbcb1f95e3a3e7fc50
+size 2820
---
+++ homework/JS/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13c3256480e0b4c4934ad3b8c442fc9fbe3eb66bec5ffd4a7437703fa8f32a87
+size 620
